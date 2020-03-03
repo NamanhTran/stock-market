cc97575b9ef52a9c5356d2c91a8f1453f59bc094
ryanwalton <31267577+ryanwalton@users.noreply.github.com>
2020-03-03 15:45:40 -0800

    Update database_queries.py

--- database_queries.py
+++ database_queries.py
@@ -9,9 +9,9 @@ mycursor = db.cursor()
 
 username = /*Needs to get username from the html, not sure how to implement*/
 
-/*selects the row in STOCK where the userID matches the username, will return the stockid, how many shares and its' price*/ 
-mycursor.execute("SELECT stockID, quantity, market_value
+/*selects the rows in STOCK where the userID matches the username, will return the stockid, how many shares and its price*/
+mycursor.execute("SELECT stockID, quantity, market_value 
                  FROM STOCK
                  WHERE userID = "%s"
-                 ORDER BY blank DESC", username)
-
+                 ORDER BY stockID ASC", username)
+                 
