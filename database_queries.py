d5487206c8c7ec4afee66d0218687680ce9c7cf5
ryanwalton <31267577+ryanwalton@users.noreply.github.com>
2020-03-03 15:13:03 -0800

    Update and rename database_queries.sql to database_queries.py

---
+++ database_queries.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+db = mysql.connector.connect(host = "",
+                             user = "",
+                            passwd = "",
+                            database = "STOCK_TRADER")
+                            
+mycursor = db.cursor()
+
+username = /*Needs to get username from the html, not sure how to implement*/
+
+/*selects the row in STOCK where the userID matches the username, will return the stockid, how many shares and its' price*/ 
+mycursor.execute("SELECT stockID, quantity, market_value
+                 FROM STOCK
+                 WHERE userID = "%s"
+                 ORDER BY blank DESC", username)
+
