d73a6783f9896e61f76060bb0f588451b14a629d
ryanwalton <31267577+ryanwalton@users.noreply.github.com>
2020-03-04 13:07:49 -0800

    Update database_queries.py

--- database_queries.py
+++ database_queries.py
@@ -1,9 +1,9 @@
 import mysql.connector
 
-db = mysql.connector.connect(host = "",
-                             user = "",
-                            passwd = "",
-                            database = "STOCK_TRADER")
+db = mysql.connector.connect(host = '',
+                             user = '',
+                            passwd = '',
+                            database = 'STOCK_TRADER'
                             
 mycursor = db.cursor()
 
@@ -12,6 +12,6 @@ username = /*Needs to get username from the html, not sure how to implement*/
 /*selects the rows in STOCK where the userID matches the username, will return the stockid, how many shares and its price*/
 mycursor.execute("SELECT stockID, quantity, market_value 
                  FROM STOCK
-                 WHERE userID = "%s"
+                 WHERE userID = '%s'
                  ORDER BY stockID ASC", username)
                  
