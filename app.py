79f88904d63901ef7fa23cc3ec990c67eda92456
Namanh Tran <namanhdtran@gmail.com>
2020-03-08 12:49:12 -0700

    Added Brain's HTML pages and Greg's stock charts

--- app.py
+++ app.py
@@ -1,32 +1,47 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import redirect
+from alpha_vantage.timeseries import TimeSeries
 
 app = Flask(__name__)
-
-@app.route("/")
+# H2AKUMZSFASPIHZH
+api = TimeSeries(key='H2AKUMZSFASPIHZH')
+# Home page
+@app.route('/')
 def home():
+    print(api.get_monthly('GOOGL'))
     return render_template('home.html')
 
-@app.route("/portfolio")
+# Portfolio page login required
+@app.route('/portfolio')
 def portfolio():
     return render_template('portfolio.html')
 
+# Buy page login required
 @app.route("/buy", methods=["GET", "POST"])
 def buy():
     if request.method == "GET":
         return render_template('buy.html')
+    
+    elif request.method == "POST":
+        data = request.form.get('input')
+        print(api.get_quote_endpoint(symbol=data))
+        return redirect('/buy')
 
+# Sell page login requried
 @app.route("/sell", methods=["GET", "POST"])
 def sell():
     if request.method == "GET":
         return render_template('sell.html')
 
+# login page
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
         return render_template('login.html')
 
+# register page 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "GET":
